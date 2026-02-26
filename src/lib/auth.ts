becc6221c6195459d4b26b3bdf20880cc1622034
Safuan Hakim <155789635+safcoding@users.noreply.github.com>
2026-02-26 16:30:58 +0800

    updated database connection

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,9 +1,14 @@
 import { betterAuth } from 'better-auth'
 import { tanstackStartCookies } from 'better-auth/tanstack-start'
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { PrismaClient } from "@/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+const prisma = new PrismaClient({ adapter });
 export const auth = betterAuth({
-  emailAndPassword: {
-    enabled: true,
-  },
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
   plugins: [tanstackStartCookies()],
 })
