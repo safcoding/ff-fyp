8565d0de1b7566332a20ade63bfab8fbc9fb1d02
Safuan Hakim <155789635+safcoding@users.noreply.github.com>
2026-05-31 23:12:12 +0800

    updated code for auth routes

--- src/routes/admin.tsx
+++ src/routes/admin.tsx
@@ -1,30 +1,23 @@
-import { createFileRoute, Outlet, useNavigate } from '@tanstack/react-router'
-import { useEffect } from 'react'
-import { signOut, useSession } from '@/lib/auth-client'
+import { createFileRoute, Outlet, redirect } from '@tanstack/react-router'
+import { signOut } from '@/lib/auth-client'
 import { Button } from '@/components/ui/button'
-signOut
+import { getSessionFn } from '@/lib/auth-middleware'
+
 export const Route = createFileRoute('/admin')({
+  beforeLoad: async() => {
+    const session = await getSessionFn();
+    if(!session?.user){
+      throw redirect({to: "/login"});
+    }
+  },
   component: AdminLayout,
 })
 
 function AdminLayout() {
-  const navigate = useNavigate()
-  const { data: session, isPending } = useSession()
-
-  useEffect(() => {
-    if ( !session?.user) {
-      void navigate({ to: '/login' })
-    }
-  }, [ isPending, session, navigate])
-  if (isPending) return <div>Checking session...</div>
-  if (!session?.user) {
-    return null
-  }
 
   return (  
   <div>
-    <p>Client signed in as {session?.user.name}</p>
-    {session && <Button onClick={() => signOut()}>Sign out</Button>}
+    <Button onClick={() => signOut()}>Sign out</Button>
     <Outlet />
   </div>)
 }
